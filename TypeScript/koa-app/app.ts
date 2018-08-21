1194ac72d42d75bfff6eca34cb1d69110dc25a64
forsigner <forsigner@gmail.com>
2018-08-21 17:41:17 +0800

    feat(TypeScript): can debug koa-app

---
+++ TypeScript/koa-app/app.ts
@@ -0,0 +1,12 @@
+import * as Koa from 'koa';
+
+const app = new Koa();
+
+app.use(async ctx => {
+  console.log('debug');
+  ctx.body = 'Hello World';
+});
+
+app.listen(3000, () => {
+  console.log('server is running');
+});
